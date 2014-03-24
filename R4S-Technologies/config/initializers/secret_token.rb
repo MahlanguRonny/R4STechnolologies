717f20475de46dea3e1722750db311bb88ee1cec
MahlanguRonny <MahlanguRonny@gamil.com>
2014-03-24 22:17:49 +0200

    first commit

---
+++ R4S-Technologies/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+R4STechnologies::Application.config.secret_key_base = 'c9010004f241aec38a9753d55b9d97409fcc3e8d3b5bff5084f85d4127c0889380df21cef051f2d1397b9a7b3d33366b2041be74d871a598778c3b2bbf0174ea'
